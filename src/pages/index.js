cd477e8e65169d9cc8593e1b52a70e8ff7f04fa6
Adlai <78224484+adlaikohn1415@users.noreply.github.com>
2022-11-17 16:35:32 -0700

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,8 +12,7 @@ export default function Home() {
         <h1 className='title'>Congratulations! You've set up your webapp!</h1>
 
         <p className='description'>
-          Get started by editing <code>src/pages/index.js</code> to be your
-          project home page.
+          This is our homepage.
         </p>
         <p className='description2'>
           Next, each team member will create their own about me file in the
